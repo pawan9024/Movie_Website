767103b8b24e4de73f2901a36c87672353b6432a
pawan9024 <pawankumaryadavvns123@gmail.com>
2025-07-23 21:27:19 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,8 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
 	plugins: [react()],
 	server: {
-		proxy: {
-			"/api":'http://localhost:5000',
-		},
+		// proxy: {
+		// 	"/api":'http://localhost:5000',
+		// },
+		port:3000,
 	}, 
 });
