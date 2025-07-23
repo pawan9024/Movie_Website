fdd9eda192bfba4d7dd3c50cfc29314936463b8c
pawan9024 <pawankumaryadavvns123@gmail.com>
2025-07-23 20:27:20 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ app.listen(port, () => {
 	console.log("Server is Running PORT No. " + port);
 	connectDB();
 });
-app.listen(5000, '0.0.0.0', () => {
+app.listen(port, '0.0.0.0', () => {
 	console.log('Server running on port 5000');
   });
   
